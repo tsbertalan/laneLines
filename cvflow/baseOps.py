Fix scaling bug in image plotter ...
But then realize that there's no reasonable way to convert real-valued
data to uint8, without imposing a normalization that might not be
desirable.

cvflow/baseOps.py
@@ -351,11 +351,12 @@ class EqualTo(Boolean):
 
 class AsType(Op):
 
-    def __init__(self, parent, kind, scaleUintTo255=False, **kwargs):
+    def __init__(self, parent, kind, scaleUintTo255=False, maxDivisor=None, **kwargs):
         self.hidden = True
         self.addParent(parent)
         self.kind = kind
         self.scaleUintTo255 = scaleUintTo255
+        self.maxDivisor = maxDivisor
         super().__init__(**kwargs)
         self.node_properties['shape'] = 'circle'
 
@@ -365,7 +366,7 @@ class AsType(Op):
         if self.kind == 'uint8' or self.kind == np.uint8 and self.scaleUintTo255:
             inarray = inarray.astype('float64')
             inarray -= inarray.min()
-            m = inarray.max()
+            m = self.maxDivisor if self.maxDivisor is not None else inarray.max()
             if m != 0:
                 inarray /= m
                 inarray *= 255

cvflow/misc.py
@@ -390,13 +390,18 @@ class CvMultiPlot:
         drPix, dcPix = self.getResizedShape(row, col)
 
         # Get the dtype and output range right.
+        image = np.copy(image)
         if image.dtype != np.uint8:
-            if image.dtype == float:
+            if image.dtype in [float, np.float32, np.float64]:
                 image -= image.min()
-                image /= image.max()
+                m = image.max()
+                if m > 1:
+                    from warnings import warn
+                    warn('Using a non-unity scaling for float image data. This might cause flashing in output.')
+                image /= m if m > 1 else 1
             else:
                 assert image.dtype == bool
-            image = image.astype('uint8') * 255
+            image = (image*255).astype('uint8')
 
         # Apply colormaps and tilings to mono and boolean images.
         image = image.reshape((image.shape[0], image.shape[1], -1))